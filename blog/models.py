d6f68969ec1363478024d4873d5ea98dd090c6a7
Kabylbekova <kabylbekovaburul@gmail.com>
2026-06-04 17:44:47 +0600

    Fix: rename Blog to Task with proper ToDo fields

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
-class Blog(models.Model):
-    name = models.CharField(max_length=250)
-    last_name = models.CharField(max_length=250)
-    age = models.IntegerField()
-    
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
-        return f'name {self.name}'
+        return self.title
 
