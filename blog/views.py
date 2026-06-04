10a1bc8f07c5284af69777190675ed80640db7cc
Kabylbekova <kabylbekovaburul@gmail.com>
2026-06-04 17:44:58 +0600

    Fix: rename to TaskViewSet, order by created_at

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
-from .models import Blog
 from rest_framework import viewsets
-from .serializers import BlogSerializers 
+from .models import Task
+from .serializers import TaskSerializer
 
-class ViewBlog(viewsets.ModelViewSet):
-    queryset = Blog.objects.all()
-    serializer_class = BlogSerializers
-    
-    
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all().order_by("-created_at")
+    serializer_class = TaskSerializer
+
